d0c5457424190230d52f86c9fdf8807c8d7cefb6
Dave Thaler <dthaler1968@gmail.com>
2026-04-13 09:01:36 -0700

    Fix stream.sh issues
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- node/stream.sh
+++ node/stream.sh
@@ -29,8 +29,9 @@ echo $timestamp > /tmp/$NODE_NAME/latest.txt
 
 STREAM_RATE=48000
 
-if [ -z ${SAMPLE_RATE+48000} ]; then
+if [ -z "${SAMPLE_RATE}" ]; then
     echo "setting sampling rate to 48000"
+    SAMPLE_RATE=48000
 else
     echo "sample rate is set to $SAMPLE_RATE";
 fi
@@ -46,7 +47,7 @@ echo "Node started at $timestamp"
 echo "Node is named $NODE_NAME and is of type $NODE_TYPE"
 ## NODE_TYPE set in .env filt to one of: "research"; "debug" (DASH-only); "hls-only"; or default (FLAC+HLS+DASH) 
 
-if [ $NODE_TYPE = "research" ]; then
+if [ "$NODE_TYPE" = "research" ]; then
         #SAMPLE_RATE=192000
 	## Setup Jack Audio outside for now
 	# sudo echo @audio - memlock 256000 >> /etc/security/limits.conf
@@ -59,7 +60,7 @@ if [ $NODE_TYPE = "research" ]; then
        -f segment -segment_time "00:00:$FLAC_DURATION.00" -strftime 1 "/tmp/$NODE_NAME/flac/%Y-%m-%d_%H-%M-%S_$NODE_NAME-$SAMPLE_RATE-$CHANNELS.flac" \
        -f segment -segment_list "/tmp/$NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time $SEGMENT_DURATION -segment_format \
        mpegts -ar $STREAM_RATE -ac 2 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" >/dev/null 2>/dev/null &
-elif [ $NODE_TYPE = "debug" ]; then
+elif [ "$NODE_TYPE" = "debug" ]; then
 	echo "Sampling $CHANNELS channels from $AUDIO_HW_ID at $SAMPLE_RATE Hz with bitrate of 32 bits/sample..."
         echo "Asking ffmpeg to stream DASH via mpegts at $STREAM_RATE Hz..." 
   	## Streaming DASH only via mpegts
@@ -67,12 +68,12 @@ elif [ $NODE_TYPE = "debug" ]; then
   	#### Stream with test engine live tools
 	## May need to adjust segment length in config_audio.json to match $SEGMENT_DURATION...
   	nice -n -7 ./test-engine-live-tools/bin/live-stream -c ./config_audio.json udp://127.0.0.1:1234 &
-elif [ $NODE_TYPE = "hls-only" ]; then
+elif [ "$NODE_TYPE" = "hls-only" ]; then
 	echo "Sampling $CHANNELS channels from $AUDIO_HW_ID at $SAMPLE_RATE Hz..."
   	echo "Asking ffmpeg to stream only HLS segments at $STREAM_RATE Hz......" 
   	## Streaming HLS only via mpegts
 	nice -n -10 ffmpeg -f jack -i ffjack -f segment -segment_list "/tmp/$NODE_NAME/hls/$timestamp/live.m3u8" -segment_list_flags +live -segment_time $SEGMENT_DURATION -segment_format mpegts -ar $STREAM_RATE -ac $CHANNELS -threads 3 -acodec aac "/tmp/$NODE_NAME/hls/$timestamp/live%03d.ts" &
-elif [ $NODE_TYPE = "dev-virt-s3" ]; then
+elif [ "$NODE_TYPE" = "dev-virt-s3" ]; then
     SAMPLE_RATE=48000
     STREAM_RATE=48000
   echo "Sampling from $AUDIO_HW_ID at $SAMPLE_RATE Hz..."
@@ -90,17 +91,17 @@ sleep 3
 jack_connect system:capture_1 ffjack:input_1
 jack_connect system:capture_2 ffjack:input_2
 
-if [ $NODE_LOOPBACK = "true" ]; then
+if [ "$NODE_LOOPBACK" = "true" ]; then
     jack_connect system:capture_1 system:playback_1
     jack_connect system:capture_2 system:playback_2
 fi
 
-if [ $NODE_LOOPBACK = "hls" ]; then
+if [ "$NODE_LOOPBACK" = "hls" ]; then
     sleep 20
     ffplay -nodisp /tmp/$NODE_NAME/hls/$timestamp/live.m3u8    
 fi
 
-if [ $NODE_TYPE = "research" ]; then
+if [ "$NODE_TYPE" = "research" ]; then
     python3 upload_s3.py &
     python3 upload_flac_s3.py
 else
